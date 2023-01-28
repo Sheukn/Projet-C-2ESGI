fd74c2807b1f5e3b77f47d0def143313bfc0818c
sheukn <steven.chouk@gmail.com>
2023-01-28 18:06:41 +0100

    Modifying some struct and adding a weird enemy detection

--- army_init.h
+++ army_init.h
@@ -1,130 +1,52 @@
-player player1; 
-player player2;
-
-player init_player1(){
-    player1.id = 1;
-    player1.remaining_units = 10;
+void init_player(player* player, int id){
+    player->id = id;
+    player->remaining_units = 10;
+    player->army = malloc(sizeof(unit) * 10);
     for(int i = 0; i < 10; i++){
-        if(i < 2){ // knight
-            player1.army[i].type = 0;
-            player1.army[i].pos.x = 0;
-            player1.army[i].pos.y = 0;
-            player1.army[i].hp = 100;
-            player1.army[i].atk = 10;
-            player1.army[i].def = 10;
-            player1.army[i].weak = 1;  // weak against archer
-            player1.army[i].speed = 10;
-            player1.army[i].range = 1;
-        }
-        else if (i < 4){ // archer
-            player1.army[i].type = 1;
-            player1.army[i].pos.x = 0;
-            player1.army[i].pos.y = 0;
-            player1.army[i].hp = 100;
-            player1.army[i].atk = 10;
-            player1.army[i].def = 10;
-            player1.army[i].weak = 2; // weak against horseman
-            player1.army[i].speed = 10;
-            player1.army[i].range = 2;
-        }   
-        else if (i < 6){ // horseman
-            player1.army[i].type = 2;
-            player1.army[i].pos.x = 0;
-            player1.army[i].pos.y = 0;
-            player1.army[i].hp = 100;
-            player1.army[i].atk = 10;
-            player1.army[i].def = 10;
-            player1.army[i].weak = 4; // weak against spearman
-            player1.army[i].speed = 10;
-            player1.army[i].range = 1;
-        }
-        else if (i < 8){ // catapult
-            player1.army[i].type = 3;
-            player1.army[i].pos.x = 0;
-            player1.army[i].pos.y = 0;
-            player1.army[i].hp = 100;
-            player1.army[i].atk = 10;
-            player1.army[i].def = 10;
-            player1.army[i].weak = 2; // weak against horseman
-            player1.army[i].speed = 10;
-            player1.army[i].range = 3;
-        }
-        else{ // spearman
-            player1.army[i].type = 4;
-            player1.army[i].pos.x = 0;
-            player1.army[i].pos.y = 0;
-            player1.army[i].hp = 100;
-            player1.army[i].atk = 10;
-            player1.army[i].def = 10;
-            player1.army[i].weak = 1; // weak against archer
-            player1.army[i].speed = 10;
-            player1.army[i].range = 1;
-        }
-        
-    }
-}
-
-
-//Same as init_player1 but for player2
-
-player init_player2(){
-    player2.id = 2;
-    player2.remaining_units = 10;
-    for(int i = 0; i < 10; i++){
-        if(i < 2){ // knight
-            player2.army[i].type = 0;
-            player2.army[i].pos.x = 0;
-            player2.army[i].pos.y = 0;
-            player2.army[i].hp = 100;
-            player2.army[i].atk = 10;
-            player2.army[i].def = 10;
-            player2.army[i].weak = 1; // weak against archer
-            player2.army[i].speed = 3;
-            player2.army[i].range = 1;
-        }
-        else if (i < 4){ // archer
-            player2.army[i].type = 1;
-            player2.army[i].pos.x = 0;
-            player2.army[i].pos.y = 0;
-            player2.army[i].hp = 100;
-            player2.army[i].atk = 10;
-            player2.army[i].def = 10;
-            player2.army[i].weak = 2; // weak against horseman
-            player2.army[i].speed = 3;
-            player2.army[i].range = 2;
-        }   
-        else if (i < 6){ // horseman
-            player2.army[i].type = 2;
-            player2.army[i].pos.x = 0;
-            player2.army[i].pos.y = 0;
-            player2.army[i].hp = 100;
-            player2.army[i].atk = 10;
-            player2.army[i].def = 10;
-            player2.army[i].weak = 4; // weak against spearman
-            player2.army[i].speed = 5;
-            player2.army[i].range = 1;
-        }
-        else if (i < 8){ // catapult
-            player2.army[i].type = 3;
-            player2.army[i].pos.x = 0;
-            player2.army[i].pos.y = 0;
-            player2.army[i].hp = 100;
-            player2.army[i].atk = 10;
-            player2.army[i].def = 10;
-            player2.army[i].weak = 2; // weak against horseman
-            player2.army[i].speed = 2;
-            player2.army[i].range = 3;
-        }
-        else{ // spearman
-            player2.army[i].type = 4;
-            player2.army[i].pos.x = 0;
-            player2.army[i].pos.y = 0;
-            player2.army[i].hp = 100;
-            player2.army[i].atk = 10;
-            player2.army[i].def = 10;
-            player2.army[i].weak = 1; // weak against archer
-            player2.army[i].speed = 4;
-            player2.army[i].range = 1;
-        }
+        if(i < 2){ 
+            player->army[i].type = 0;
+            player->army[i].hp = 100;
+            player->army[i].atk = 10;
+            player->army[i].def = 10;
+            player->army[i].weak = 1; // Weak against archer
+            player->army[i].speed = 3;
+            player->army[i].range = 1;
+        } // Knights
+        else if(i < 4){
+            player->army[i].type = 1;
+            player->army[i].hp = 100;
+            player->army[i].atk = 10;
+            player->army[i].def = 5;
+            player->army[i].weak = 2; // Weak against horseman
+            player->army[i].speed = 3;
+            player->army[i].range = 2;
+        } // Archers
+        else if(i < 6){
+            player->army[i].type = 2;
+            player->army[i].hp = 100;
+            player->army[i].atk = 10;
+            player->army[i].def = 5;
+            player->army[i].weak = 4; // Weak against spearman
+            player->army[i].speed = 5;
+            player->army[i].range = 1;
+        } // Horseman
+        else if(i < 8){
+            player->army[i].type = 3;
+            player->army[i].hp = 100;
+            player->army[i].atk = 10;
+            player->army[i].def = 5;
+            player->army[i].weak = 2; // Weak against horseman
+            player->army[i].speed = 2;
+            player->army[i].range = 5;
+        } // Catapults
+        else {
+            player->army[i].type = 4;
+            player->army[i].hp = 100;
+            player->army[i].atk = 10;
+            player->army[i].def = 5;
+            player->army[i].weak = 1; // Weak against archer
+            player->army[i].speed = 2;
+            player->army[i].range = 1;
+        } // Spearman
     }
 }
--- main.c
+++ main.c
@@ -1,15 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <SDL2/SDL.h>
+#include <conio.h>
+#include <windows.h>
+#include <stdbool.h>
 #include "struct.h"
-#include "detectionTest.h"
 #include "army_init.h"
+#include "detectionTest.h"	
 
-
-
-int main(int argc, char *argv[])
+int main()
 {
-    player player1 = init_player1();
-    player player2 = init_player2();
-    isReachable(&player1.army[5]);
+
+    // Initialize the army
+    player* player1;
+    player1 = malloc(sizeof(player));
+    init_player(player1, 1);
+
+    // Create grid 20x20 with ' ' as empty space
+    char **grid;
+    grid = malloc(sizeof(char*) * 20);
+    for(int i = 0; i < 20; i++){
+        grid[i] = malloc(sizeof(char) * 20);
+        for(int j = 0; j < 20; j++){
+            grid[i][j] = ' ';
+        }
+    }
+
+
+    //Create walls '#'
+    for(int i = 0; i < 20; i++){
+        grid[i][0] = '#';
+        grid[i][19] = '#';
+        grid[0][i] = '#';
+        grid[19][i] = '#';
+    }
+
+
+    // Place the unit
+    player1->army[3].pos.x = 3;
+    player1->army[3].pos.y = 3;
+    grid[player1->army[3].pos.x][player1->army[3].pos.y] = 'A';
+    printf("Player 1's first unit's x position: %d\n", player1->army[3].pos.x);
+    printf("Player 1's first unit's y position: %d\n", player1->army[3].pos.y);
+
+    
+
+    //place the enemy
+    grid[5][5] = 'E';
+    grid[10][10] = 'E';
+    grid[1][1] = 'E';
+
+   
+
+    while(1){
+        
+        // Print the grid
+        printf("Player 1's first unit's range: %d\n", player1->army[3].range);
+        for(int i = 0; i < 20; i++){
+            for(int j = 0; j < 20; j++){
+                printf("%c ", grid[i][j]);
+            }
+            printf("\n");
+        }
+        switch(getch()){
+            case 'a':
+                if(grid[player1->army[3].pos.x][player1->army[3].pos.y - 1] == ' '){
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = ' ';
+                    player1->army[3].pos.y -= 1;
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = 'A';
+                }
+                break;
+            case 'd':
+                if(grid[player1->army[3].pos.x][player1->army[3].pos.y + 1] == ' '){
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = ' ';
+                    player1->army[3].pos.y += 1;
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = 'A';
+                }
+                break;
+            case 'w':
+                if(grid[player1->army[3].pos.x - 1][player1->army[3].pos.y] == ' '){
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = ' ';
+                    player1->army[3].pos.x -= 1;
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = 'A';
+                }
+                break;
+            case 's':
+                if(grid[player1->army[3].pos.x + 1][player1->army[3].pos.y] == ' '){
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = ' ';
+                    player1->army[3].pos.x += 1;
+                    grid[player1->army[3].pos.x][player1->army[3].pos.y] = 'A';
+                }
+                break;
+            case 'q':
+                free(player1->army);
+                free(player1);
+                free(grid);
+                return 0;
+        }
+        system("cls");
+        isReachable(player1->army[3], grid);
+    }
 }
